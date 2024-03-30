beeeaaf705549617f005dd7fc384c02785042515
ananya-sh30 <142817946+ananya-sh30@users.noreply.github.com>
2024-03-30 14:02:30 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,112 @@
+var score=0;
+var noOfCol= 6;
+for(var i=0; i<noOfCol; i++){
+        document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[0].addEventListener("click", function(){
+            score=score+1;
+        
+    });
+    document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[1].addEventListener("click", function(){
+        score=score+2;
+    
+    });
+    document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[2].addEventListener("click", function(){
+        score=score+3;
+
+    });
+    document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[3].addEventListener("click", function(){
+        score=score+4;
+
+    });
+    document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[4].addEventListener("click", function(){
+        score=score+5;
+
+    });
+}
+for(var i=0; i<noOfCol; i++){
+    document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[0].addEventListener("click", function(){
+        score=score+1;
+    
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[1].addEventListener("click", function(){
+    score=score+2;
+
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[2].addEventListener("click", function(){
+    score=score+3;
+
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[3].addEventListener("click", function(){
+    score=score+4;
+
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[4].addEventListener("click", function(){
+    score=score+5;
+
+});
+}
+for(var i=0; i<noOfCol; i++){
+    document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[0].addEventListener("click", function(){
+        score=score+1;
+    
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[1].addEventListener("click", function(){
+    score=score+2;
+
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[2].addEventListener("click", function(){
+    score=score+3;
+
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[3].addEventListener("click", function(){
+    score=score+4;
+
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[4].addEventListener("click", function(){
+    score=score+5;
+
+});
+}
+for(var i=0; i<noOfCol; i++){
+    document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[0].addEventListener("click", function(){
+        score=score+1;
+    
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[1].addEventListener("click", function(){
+    score=score+2;
+
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[2].addEventListener("click", function(){
+    score=score+3;
+
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[3].addEventListener("click", function(){
+    score=score+4;
+
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[4].addEventListener("click", function(){
+    score=score+5;
+
+});
+}
+for(var i=0; i<noOfCol; i++){
+    document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[0].addEventListener("click", function(){
+        score=score+1;
+    
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[1].addEventListener("click", function(){
+    score=score+2;
+
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[2].addEventListener("click", function(){
+    score=score+3;
+
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[3].addEventListener("click", function(){
+    score=score+4;
+
+});
+document.querySelectorAll(".col")[i].querySelector(".rate").querySelectorAll("button")[4].addEventListener("click", function(){
+    score=score+5;
+
+});
+}
